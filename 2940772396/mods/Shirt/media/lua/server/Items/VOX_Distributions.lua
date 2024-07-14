38df44dadb58021b41457c1ae50dd45ee2f56ccb
aguja262 <124748378+aguja262@users.noreply.github.com>
2024-07-14 05:46:11 +0200

    para editar

---
+++ 2940772396/mods/Shirt/media/lua/server/Items/VOX_Distributions.lua
@@ -0,0 +1,11 @@
+require 'Items/ProceduralDistributions'
+
+
+table.insert(ProceduralDistributions.list["WardrobeMan"].items, "VOX.Shirt_VOX");
+table.insert(ProceduralDistributions.list["WardrobeMan"].items, 5);
+
+table.insert(ProceduralDistributions.list["WardrobeWoman"].items, "VOX.Shirt_VOX");
+table.insert(ProceduralDistributions.list["WardrobeWoman"].items, 5);
+
+table.insert(ProceduralDistributions.list["BedroomDresser"].items, "VOX.Shirt_VOX");
+table.insert(ProceduralDistributions.list["BedroomDresser"].items, 5);
